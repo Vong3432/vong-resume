fde585d07ac41e37b820098c40646a580d9439c1
Vong3432 <36991424+Vong3432@users.noreply.github.com>
2018-08-09 19:33:16 +0800

    addMalaysiafoodandLoginPage

--- myFood.js
+++ myFood.js
@@ -7,3 +7,14 @@ function openHamburger()
         x.className = "topnav";
     }
 }
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("myTopnav").style.top = "0";
+  } else {
+    document.getElementById("myTopnav").style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
+}
