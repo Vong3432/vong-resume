385a3c581eb3b1f727b05febb913c7819855f7fb
Vong3432 <vongnyuksoon2000@gmail.com>
2018-08-10 19:21:41 +0800

    updateformalaysiafood

--- myFood.js
+++ myFood.js
@@ -1,11 +1,15 @@
 function openHamburger()
 {
 	var x = document.getElementById("myTopnav");
+
     if (x.className === "topnav") {
         x.className += " responsive";
     } else {
         x.className = "topnav";
     }
+
+document.body.scrollTop = 0; // For Safari
+document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera  
 }
 
 var prevScrollpos = window.pageYOffset;
@@ -14,7 +18,7 @@ window.onscroll = function() {
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("myTopnav").style.top = "0";
   } else {
-    document.getElementById("myTopnav").style.top = "-100px";
+    document.getElementById("myTopnav").style.top = "-110px";
   }
   prevScrollpos = currentScrollPos;
 }
