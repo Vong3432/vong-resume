c128d84ad8b3ae4651c84f304afc1e966c4d0bd0
Vong3432 <36991424+Vong3432@users.noreply.github.com>
2018-08-23 16:01:22 +0800

    insert svg

--- malaysiabeta.js
+++ malaysiabeta.js
@@ -23,3 +23,12 @@ function openNav()
         title.style.display = "block";
     }
 }*/
+var lineDrawing = anime({
+  targets: '#lineDrawing .lines path',
+  strokeDashoffset: [anime.setDashoffset, 0],
+  easing: 'easeInOutSine',
+  duration: 1500,
+  delay: function(el, i) { return i * 250 },
+  direction: 'alternate',
+  loop: true
+});
