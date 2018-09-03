2990cd069b1c8ebd2debf0c43af833aad89b6181
Vong3432 <36991424+Vong3432@users.noreply.github.com>
2018-09-03 21:35:29 +0800

    animate.css first time

--- port.js
+++ port.js
@@ -3,4 +3,24 @@ $(document).ready(function(){
 		hover:true
 	});
 	$('.parallax').parallax();
+	$('#aboutZoomIn').hide();
+	$('.introduce-icon').hide();
+	$('.carousel.carousel-slider').carousel({
+		//  anything to set
+	}).hide();
+});
+
+$(document).on('scroll', function() {
+    if($(this).scrollTop()>=$('.card').position().top){
+    	$('#aboutZoomIn').show();
+    	$('.introduce-icon').show();    	
+        $('#aboutZoomIn').addClass('animated zoomIn slow'); 
+        $('.introduce-icon').addClass('animated fadeInLeft slow'); 
+
+        // For carousel fadeInUp effect
+        if($(this).scrollTop()>$('.introduce-icon').position().top){
+    		$('.carousel.carousel-slider').show();
+    		$('.carousel.carousel-slider').addClass('animated fadeInUp slower delay-3s');
+    	}
+    }  	    
 });
